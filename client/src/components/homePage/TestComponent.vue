168cf1522734e58d9505d0f8c3b9e70ea33c98f0
Nick <nicksonogm@gmail.com>
2026-01-02 09:43:03 +0300

    already set up the client ready for communication with the server. About to check the cors implementation of the server
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>

---
+++ client/src/components/homePage/TestComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Checking server connection...</p>
+    <p v-if="serverResponse">Server Response: {{ serverResponse }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+const serverResponse = ref('')
+
+const testServer = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/status')
+    console.log('Server response:', response.data)
+    serverResponse.value = response.data.message;
+  } catch (error) {
+    console.error('Error connecting to server:', error)
+  }
+}
+
+// This hook triggers as soon as the component is added to the DOM
+onMounted(() => {
+  testServer()
+})
+</script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import TestComponent from '@/components/homePage/TestComponent.vue';
 </script>
 
 <template>
   <main>
-
+    <h1>Welcome to the Home Page </h1>
+    <TestComponent />
   </main>
 </template>
