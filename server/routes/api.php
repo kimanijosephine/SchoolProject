e6b636a7356bacc1c89cacb583ed687a05afe376
Nick <nicksonogm@gmail.com>
2026-01-02 17:33:58 +0300

    added a route for uploading csv

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\School\StudentImportController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,5 +20,5 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('auth:school')->group(function () {
-    // Route::post('/students/import', [StudentImportController::class, 'import']);
+    Route::post('/students/import', [StudentImportController::class, 'import']);
 });
