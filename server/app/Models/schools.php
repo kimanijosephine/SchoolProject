2ddf9d9d983bc3ccccc02d70a9fa5f65231a54f4
Nick <nicksonogm@gmail.com>
2026-01-02 12:58:17 +0300

    modified schools model
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>

--- server/app/Models/schools.php
+++ server/app/Models/schools.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class schools extends Model
+class schools extends Authenticatable
 {
-    //
+    protected $fillable = ['name', 'email', 'password', 'id'];
+    protected $hidden = ['password'];
 }
