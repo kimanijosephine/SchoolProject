591e6a93bbd2082c35210b6c232457eefb0deea5
Nick <nicksonogm@gmail.com>
2026-01-02 13:15:32 +0300

    modified schools and sponsors model, almost done with sponor
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>
    
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>

--- server/app/Models/schools.php
+++ server/app/Models/schools.php
@@ -2,11 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class schools extends Authenticatable
+class Schools extends Authenticatable implements JWTSubject
 {
-    protected $fillable = ['name', 'email', 'password', 'id'];
-    protected $hidden = ['password'];
+    use Notifiable;
+
+    protected $fillable = [ 'name', 'email', 'password'];
+    protected $hidded = ['password'];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'role' => 'school' // This helps identify the user type in Vue
+        ];
+    }
 }
