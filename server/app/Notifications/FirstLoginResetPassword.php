6e42c9032c0b8ce3c6409e487bfc326307b69fbc
Nick <nicksonogm@gmail.com>
2026-01-14 16:35:21 +0300

    Modified login controller to have a logic for first time student login
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>
    
    Co-authored-by: Josephine Kimani <kimanijosephine62@gmail.com>

--- server/app/Notifications/FirstLoginResetPassword.php
+++ server/app/Notifications/FirstLoginResetPassword.php
@@ -1,54 +1,34 @@
 <?php
-
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class FirstLoginResetPassword extends Notification
 {
-    use Queueable;
+    public $token;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
+    public function __construct($token)
     {
-        //
+        $this->token = $token;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
-    public function via(object $notifiable): array
+    public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail($notifiable)
     {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
+        // You can point this to your Vue frontend URL
+        $url = url(config('app.url').'/reset-password/'.$this->token.'?email='.$notifiable->getEmailForPasswordReset());
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
+        return (new MailMessage)
+            ->subject(Lang::get('Welcome! Please Set Your Password'))
+            ->line(Lang::get('Your account has been created by the administrator.'))
+            ->line(Lang::get('For security reasons, you must set a new password for your first login.'))
+            ->action(Lang::get('Set New Password'), $url)
+            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.users.expire')]));
     }
 }
