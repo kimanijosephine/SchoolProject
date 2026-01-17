8ef14b37bf69b3f189684f7236179713aa30645b
Arnold Kimani <deathqqq01@gmail.com>
2026-01-17 11:30:17 +0300

    created default basic code for sidebar and header for donors
    Co-authored-by: Nixon Ojiem <nicksonogm@gmail.com>
    
    Co-authored-by: danmait <threedaniel385@gmail.com>

---
+++ client/src/components/Donors/Components/HeaderComponent.vue
@@ -0,0 +1,8 @@
+<template lang="html">
+  <header>
+    <h1> Welcome to Our Donors Page</h1>
+  </header>
+</template>
+<script lang="ts" setup>
+
+</script>
---
+++ client/src/components/Donors/Components/SidebarComponent.vue
@@ -0,0 +1,6 @@
+<template lang="html">
+  <header>
+    <h1> Sidebar</h1>
+  </header>
+</template>
+<script lang="ts" setup></script>
